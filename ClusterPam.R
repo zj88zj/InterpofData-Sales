44b8a0c434647dd2c49cc451114a9cd50153d0ca
Jing Zhang <zj88zj@hotmail.com>
2019-01-15 19:37:16 -0500

    Update ClusterPam.R

--- ClusterPam.R
+++ ClusterPam.R
@@ -1,13 +1,11 @@
 ################## R Code for robust clustering with PAM #######################
-
 h = read.csv("hospital.csv")
 x = log(1+0.001*h[sample(4703,1000),-c(1:4,10:11)])
 pairs(x,pch=".")
 z = princomp(x)$scores[,c(1:4)]
 pairs(z)
 library(cluster)
-# If you want to use the same factors that you got from SAS
-# then save the SAS dataset as CSV using the export
+#save the SAS dataset as CSV using the export
 hout = read.csv("hout.csv")
 pairs(hout)
 pam(hout[,-c(1,5)],k=10)
